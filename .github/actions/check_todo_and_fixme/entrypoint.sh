b0829821e87e08be243c9384531c5cb2e06c605d
nozomi-iida <iida19990106@gmail.com>
2021-10-08 17:51:11 +0900

    feat: check TODO! and FIXME! comment

--- .github/actions/check_todo_and_fixme/entrypoint.sh
+++ .github/actions/check_todo_and_fixme/entrypoint.sh
@@ -1,20 +1,12 @@
 #!/bin/sh
-
-# default passed is true, if fails checks then passed is false
 passed=1
-# git diffでbase branchとhead branchを比較すると指定する必要がある
 
-todo=$(git diff --diff-filter=A | grep 'TODO!')
-# ↑これで何も取得できなければ1が，一つでも取得できれば0が返ってくる
+# TODO!またはFIXME!コメントがあるかをチェックする
+check=$(egrep -rn "TODO\!|FIXME\!" *)
+
 case $? in
-  0 ) echo $todo && passed=0;;
+  0 ) echo $check && passed=0;;
   * ) echo "No TODO!'s found";;
-esac
-
-fixme=$(git diff --diff-filter=A | grep 'FIXME!')
-case $? in
-  0 ) echo $todo && passed=0;;
-  * ) echo "No FIXME!'s found";;
 esac
 
 case $passed in
